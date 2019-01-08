194216786ccadd436d90d56be4201cedf0816646
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2019-01-08 00:41:33 +0000

    second commit

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 //set up express app
 const app = express();
 
+app.get('/api', function(req,res){
+    console.log('GET request');
+    res.send({name: 'Yoshi'});
+});
+
+
 //listen for requests
 app.listen(process.env.PORT || process.env.IP, function(){
     console.log('now listening for requests');
