75ac4829dfd191580a73d2600f419b364d994233
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2019-01-15 00:49:17 +0000

    bodyParser

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,12 @@ router.get('/ninjas', function(req, res){
 
 //add new ninja to database
 router.post('/ninjas', function(req, res){
-   res.send({type: 'POST'}); 
+   console.log(req.body);
+   res.send({
+      type: 'POST',
+      name: req.body.name,
+      rank:req.body.rank
+   }); 
 });
 
 //update ninja in database
