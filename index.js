7a353a501ed48c1380ab462621908819536b6431
Emily Teixeira <emilyteixeira.student@careerdevs.com>
2019-01-26 17:21:40 +0000

    next

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ mongoose.connect('mongodb://localhost/ninjago')
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
+
 //initialize routes
 app.use('/api', require('./routes/api'));
+
+//error handling middleware
+app.use(function(err, req, res, next){
+    //console.log(err);
+    res.status(422).send({error:err.message});
+})
 
 //listen for requests
 app.listen(process.env.PORT || process.env.IP, function(){
--- routes/api.js
+++ routes/api.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 //get a list of ninjas from the database
-router.get('/ninjas', function(req, res){
+router.get('/ninjas', function(req, res, next){
    res.send({type: 'GET'}); 
 });
 
 //add new ninja to database
-router.post('/ninjas', function(req, res){
+router.post('/ninjas', function(req, res, next){
   Ninja.create(req.body).then(function(ninja){
      res.send(ninja);
-   }); 
+   }).catch(next);
 });
 
 //update ninja in database
-router.put('/ninjas/:id', function(req, res){
+router.put('/ninjas/:id', function(req, res, next){
    res.send({type: 'PUT'}); 
 });
 
